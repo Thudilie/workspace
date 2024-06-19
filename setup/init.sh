63956cb84c85a784fa3f4032c64871f853d7a9af
Thuy-Anh Nguyen <Thuy-Anh.Nguyen@tui.com>
2024-06-19 13:46:49 +0200

    deploy to ecr

--- setup/init.sh
+++ setup/init.sh
@@ -9,7 +9,9 @@ echo "Downloading tool..."
 curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
+echo "Echo ${userarn}" 
 echo "Updating permissions"
+
 ./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
 
 echo "Cleaning up"
